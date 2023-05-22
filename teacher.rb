3ec5b9178f844db3daf2e7c408f0794b791ced27
byiringiroscar <byiringoroscar@gmail.com>
2023-05-22 15:32:37 +0200

    Add Teacher class

---
+++ teacher.rb
@@ -0,0 +1,15 @@
+include 'person'
+
+class Teacher < Person
+    attr_reader :specialization
+  
+    def initialize(name = 'Unknown', age = nil, parent_permission = true, specialization = 'General')
+      super(name, age, parent_permission)
+      @specialization = specialization
+    end
+  
+    def can_use_services?
+      true
+    end
+  end
+  

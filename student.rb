4ae7d919ad4e1dd23d8fe147125c1bae6a22cd14
byiringiroscar <byiringoroscar@gmail.com>
2023-05-22 16:13:45 +0200

    Fix linters issues

--- student.rb
+++ student.rb
@@ -1,14 +1,13 @@
 class Student < Person
-    attr_reader :classroom, :id
-  
-    def initialize(name = 'Unknown', age = nil, parent_permission = true, classroom = nil)
-      super(name, age, parent_permission)
-      @classroom = classroom
-      @id = Random.rand(1..1000)
-    end
-  
-    def play_hooky
-      "¯\\(ツ)/¯"
-    end
+  attr_reader :classroom, :id
+
+  def initialize(name = 'Unknown', age = nil, parent_permission: true, classroom: nil)
+    super(name, age, parent_permission)
+    @classroom = classroom
+    @id = Random.rand(1..1000)
   end
-  
+
+  def play_hooky
+    '¯\\(ツ)/¯'
+  end
+end

0ff66415d23f07edc1c1de365a0ca654e96439f2
byiringiroscar <byiringoroscar@gmail.com>
2023-05-23 19:11:24 +0200

    Add readme and decorate class

---
+++ base.rb
@@ -0,0 +1,25 @@
+require_relative 'nameable'
+
+class Decorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    trimmed_name = @nameable.correct_name
+    trimmed_name.length > 10 ? trimmed_name[0..9] : trimmed_name
+  end
+end
--- person.rb
+++ person.rb
@@ -1,8 +1,11 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(name = 'Unknown', age = nil, parent_permission: true)
+  def initialize(age = nil, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -12,6 +15,11 @@ class Person
   def can_use_services?
     of_age? || @parent_permission
   end
+
+  def correct_name
+    @name
+  end
+
 
   private
 
